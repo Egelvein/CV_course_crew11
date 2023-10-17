f029d36b5c1d24a98f49da64579eebaca81836d0
Vyacheslav <sisla00@ya.ru>
2023-10-17 09:15:09 +0300

    commit 1.1

--- data_preparing/data_grouping_for_yolo.py
+++ data_preparing/data_grouping_for_yolo.py
@@ -3,12 +3,7 @@ import os
 import random
 import shutil
 
-classes = ['missing_hole', 'mouse_bite', 'open_circuit',
-           'short', 'spurious_copper', '_spur_']
-
 train_percent = 0.8
-validation_percent = 0.15
-test_percent = 0.05
 
 # Archive unpack
 unzip_dir = 'VOC_PCB'
@@ -19,35 +14,29 @@ if not os.path.exists(unzip_dir):
         zip_ref.extractall()
 
 # Data group
-new_dir = 'Dataset_for_CNN'
+new_dir = 'Dataset_for_YOLO'
 
 if not os.path.exists(new_dir):
     os.mkdir(new_dir)
-    os.mkdir(new_dir + '/train')
-    os.mkdir(new_dir + '/validation')
-    os.mkdir(new_dir + '/test')
+    os.mkdir(os.path.join(new_dir, 'train'))
+    os.mkdir(os.path.join(new_dir, 'val'))
+    os.mkdir(os.path.join(new_dir, 'train', 'images'))
+    os.mkdir(os.path.join(new_dir, 'train', 'labels'))
+    os.mkdir(os.path.join(new_dir, 'val', 'images'))
+    os.mkdir(os.path.join(new_dir, 'val', 'labels'))
 
 photo_dir = unzip_dir + '/JPEGImages'
 annotation_dir = '../Labels'
 
 for filename in os.listdir(photo_dir):
-    class_dir = None
-    for class_name in classes:
-        if class_name in filename:
-            class_dir = class_name
-            break
-
-    if class_dir is None:
-        class_dir = 'other'
-
     rand_num = random.random()
 
     if rand_num < train_percent:
-        target_dir = os.path.join(new_dir, 'train', class_dir)
-    elif rand_num < train_percent + validation_percent:
-        target_dir = os.path.join(new_dir, 'validation', class_dir)
+        target_dir = os.path.join(new_dir, 'train', 'images')
+        annotation_target_dir = os.path.join(new_dir, 'train', 'labels')
     else:
-        target_dir = os.path.join(new_dir, 'test', class_dir)
+        target_dir = os.path.join(new_dir, 'val', 'images')
+        annotation_target_dir = os.path.join(new_dir, 'val', 'labels')
 
     os.makedirs(target_dir, exist_ok=True)
     source_path = os.path.join(photo_dir, filename)
@@ -58,7 +47,7 @@ for filename in os.listdir(photo_dir):
 
         annotation_file = os.path.splitext(filename)[0] + '.txt'
         annotation_source_path = os.path.join(annotation_dir, annotation_file)
-        annotation_target_path = os.path.join(target_dir, annotation_file)
+        annotation_target_path = os.path.join(annotation_target_dir, annotation_file)
 
         if os.path.exists(annotation_source_path):
             shutil.move(annotation_source_path, annotation_target_path)
