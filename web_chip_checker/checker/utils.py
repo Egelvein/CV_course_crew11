4c479d47e8df7613f921ccae705ed533d7a981e8
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2023-11-06 16:26:43 +0300

    add api connection to inference service

--- web_chip_checker/checker/utils.py
+++ web_chip_checker/checker/utils.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+import cv2
 import os
 
 
@@ -11,3 +12,15 @@ def handle_uploaded_file(file):
             destination.write(chunk)
 
     return f'{folder}/{file.name}'
+
+
+def draw_counters(image_path, preds):
+    img = cv2.imread(image_path)
+    for pred in preds:
+        contour = pred["contour"]
+        img = cv2.rectangle(img, (int(contour[0]), int(contour[1])),
+                            (int(contour[2]), int(contour[3])), (0, 255, 255), )
+
+    cv2.imshow("Bounding Rectangle", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
