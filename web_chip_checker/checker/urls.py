c3649a89531d01ceb04ba3231668dd5369210ae9
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2023-11-07 14:31:52 +0300

    add result view

--- web_chip_checker/checker/urls.py
+++ web_chip_checker/checker/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.main_camera, name='main'),
-]
+    path('results/<chip_id>', views.result, name='result'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
